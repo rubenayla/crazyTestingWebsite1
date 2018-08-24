fd076e8d2edf767dabd381fb1c7441b88bb50f6c
Rubén <ruben.jimenezmejias@gmail.com>
2018-08-24 09:34:03 +0200

    Add files via upload

---
+++ folder1/calculatePi1.py
@@ -0,0 +1,8 @@
+def calculatePi(darts = 5000):
+	import random
+	circle = 0
+	for i in range(darts):
+		if (random.random()**2 + random.random()**2) ** 0.5 < 1:
+			circle += 1
+	return(circle/darts * 4)
+print(calculatePi(10**6))
